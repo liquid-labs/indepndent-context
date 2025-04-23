d241a795fb4fc904d32be55f5c6d8536b2d3c4bb
Zane Rockenbaugh <zane@liquid-labs.com>
2025-04-23 15:34:48 -0500

    initial implementation

--- main.go
+++ main.go
@@ -2,52 +2,35 @@ package indepndent_context
 
 import (
 	"context"
+	"time"
 )
 
 type copiedContext struct {
-	context.Context
-	values map[interface{}]interface{}
+	self   context.Context
+	values context.Context
 }
 
-func NewIndependentContext(parent context.Context) *copiedContext {
+func NewDerivedContext(source context.Context) *copiedContext {
 	newCtx := &copiedContext{
-		Context: context.Background(),
-		values:  make(map[interface{}]interface{}),
+		self:   context.Background(),
+		values: source,
 	}
 
-	if parent != nil {
-		parentValues := getAllContextValues(parent)
-		for k, v := range parentValues {
-			newCtx.values[k] = v
-		}
-	}
 	return newCtx
 }
 
+func (c *copiedContext) Deadline() (deadline time.Time, ok bool) {
+	return c.self.Deadline()
+}
+
+func (c *copiedContext) Done() <-chan struct{} {
+	return c.self.Done()
+}
+
+func (c *copiedContext) Err() error {
+	return c.self.Err()
+}
+
 func (c *copiedContext) Value(key interface{}) interface{} {
-	return c.values[key]
-}
-
-func getAllContextValues(ctx context.Context) map[interface{}]interface{} {
-	values := make(map[interface{}]interface{})
-	collectValues(ctx, values)
-	return values
-}
-
-func collectValues(ctx context.Context, values map[interface{}]interface{}) {
-	if ctx == nil {
-		return
-	}
-	if val := ctx.Value(ctx); val != nil {
-		values[ctx] = val
-	}
-
-	collectValues(getParent(ctx), values)
-}
-
-func getParent(ctx context.Context) context.Context {
-	if parent, ok := ctx.(interface{ Parent() context.Context }); ok {
-		return parent.Parent()
-	}
-	return nil
+	return c.values.Value(key)
 }
